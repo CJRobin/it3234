94994843c8397f9450c70c4b15e7de50744e3b70
Corey Robinson <cojaro94@gmail.com>
2018-09-23 12:06:23 -0400

    test grab

--- app.lua
+++ app.lua
@@ -2,7 +2,17 @@ local lapis = require("lapis")
 local app = lapis.Application()
 
 app:get("/", function()
-  return "Welcome to apps new " .. require("lapis.version")
+  local csv = require("csv")
+  local year = os.date("%Y")
+  local month = os.date("%m")
+  local day = os.date("%d")
+  local hour = os.date("%H")
+  local f = csv.open("https://russellthackston.me/etl/sensordata_" .. year .. "_" .. month .. "_" .. day .. "_" .. hour .. ".csv")
+  for fields in f:lines() do
+    for i, v in ipairs(fields) do 
+      return i .. "_" .. v
+    end
+  end
 end)
 
 return app
