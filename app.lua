22f16457573b7a442a415ff4398b6de1ae849c8f
Corey Robinson <cojaro94@gmail.com>
2018-09-23 12:15:55 -0400

    addnumber

--- app.lua
+++ app.lua
@@ -6,9 +6,9 @@ app:get("/", function()
   local year = os.date("%Y")
   local month = os.date("%m")
   local day = os.date("%d")
-  local hour = os.date("%H")
+  local hour = tonumber(os.date("%H") +1)
   print(os.date("%Y_%m_%d_%H"))
-  local f = csv.open("https://russellthackston.me/etl/sensordata_" .. year .. "_" .. month .. "_" .. day .. "_" .. hour+1 .. ".csv")
+  local f = csv.open("https://russellthackston.me/etl/sensordata_" .. year .. "_" .. month .. "_" .. day .. "_" .. hour .. ".csv")
   for fields in f:lines() do
     for i, v in ipairs(fields) do
       print(i, v)
