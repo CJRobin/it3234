2c506b84d1361d8088b90d8516826280c092deee
Corey Robinson <cojaro94@gmail.com>
2018-09-23 12:14:42 -0400

    fix

--- app.lua
+++ app.lua
@@ -3,12 +3,12 @@ local app = lapis.Application()
 
 app:get("/", function()
   local csv = require("csv")
-  local year = os.date("%Y", os.time())
-  local month = os.date("%m", os.time())
-  local day = os.date("%d", os.time())
-  local hour = os.date("%H", os.time())
-  print(os.date("%Y_%m_%d_%H", os.time()))
-  local f = csv.open("https://russellthackston.me/etl/sensordata_" .. year .. "_" .. month .. "_" .. day .. "_" .. hour .. ".csv")
+  local year = os.date("%Y")
+  local month = os.date("%m")
+  local day = os.date("%d")
+  local hour = os.date("%H")
+  print(os.date("%Y_%m_%d_%H"))
+  local f = csv.open("https://russellthackston.me/etl/sensordata_" .. year .. "_" .. month .. "_" .. day .. "_" .. hour+1 .. ".csv")
   for fields in f:lines() do
     for i, v in ipairs(fields) do
       print(i, v)
