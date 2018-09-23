5a24f2266a4f3e22c98b5e271d985d48ab566441
Corey Robinson <cojaro94@gmail.com>
2018-09-23 12:18:38 -0400

    format

--- app.lua
+++ app.lua
@@ -7,8 +7,14 @@ app:get("/", function()
   local month = os.date("%m")
   local day = os.date("%d")
   local hour = tonumber(os.date("%H") +1)
-  print(hour)
-  local f = csv.open("https://russellthackston.me/etl/sensordata_" .. year .. "_" .. month .. "_" .. day .. "_" .. hour .. ".csv")
+  -- retrieve the content of a URL
+  local http = require("socket.http")
+  local body, code = http.request("https://russellthackston.me/etl/sensordata_" .. year .. "_" .. month .. "_" .. day .. "_" .. hour .. ".csv")
+  if not body then
+    error(code) 
+  end
+
+  local f = csv.open("sensordata_" .. year .. "_" .. month .. "_" .. day .. "_" .. hour .. ".csv")
   for fields in f:lines() do
     for i, v in ipairs(fields) do
       print(i, v)
