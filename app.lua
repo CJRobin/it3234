22de671a249268e0d07415108626b4077d2b36ca
Corey Robinson <cojaro94@gmail.com>
2018-09-23 11:51:14 -0400

    test

--- app.lua
+++ app.lua
@@ -2,7 +2,7 @@ local lapis = require("lapis")
 local app = lapis.Application()
 
 app:get("/", function()
-  return "Welcome to apps " .. require("lapis.version")
+  return "Welcome to apps new " .. require("lapis.version")
 end)
 
 return app
