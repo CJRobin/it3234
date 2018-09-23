c1561eac912799104bbd6775362796be4880dace
Corey Robinson <cojaro94@gmail.com>
2018-09-23 12:07:45 -0400

    print

--- app.lua
+++ app.lua
@@ -7,12 +7,14 @@ app:get("/", function()
   local month = os.date("%m")
   local day = os.date("%d")
   local hour = os.date("%H")
+  print(os.date("%Y_%m_%d_%H"))
   local f = csv.open("https://russellthackston.me/etl/sensordata_" .. year .. "_" .. month .. "_" .. day .. "_" .. hour .. ".csv")
   for fields in f:lines() do
-    for i, v in ipairs(fields) do 
-      return i .. "_" .. v
+    for i, v in ipairs(fields) do
+      print(i, v)
     end
   end
+  return "Welcome to apps new " .. require("lapis.version")
 end)
 
 return app
