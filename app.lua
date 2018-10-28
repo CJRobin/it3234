0548c617ca25b18c9d5db6cdec155837775cf054
Corey Robinson <cojaro94@gmail.com>
2018-10-28 13:56:25 -0400

    fix

--- app.lua
+++ app.lua
@@ -8,33 +8,22 @@ app:get("/", function()
   local day = os.date("%d")
   local hour = tonumber(os.date("%H") +1)
 
-  download("https://russellthackston.me/etl/sensordata_" .. year .. "_" .. month .. "_" .. day .. "_" .. hour .. ".csv", "sensordata_" .. year .. "_" .. month .. "_" .. day .. "_" .. hour .. ".csv")
 
-  -- local body, code = http.request("https://russellthackston.me/etl/sensordata_" .. year .. "_" .. month .. "_" .. day .. "_" .. hour .. ".csv")
-  -- if not body then
-  --   error(code)
-  -- end
-  -- local f = assert(io.open("sensordata_" .. year .. "_" .. month .. "_" .. day .. "_" .. hour .. ".csv", 'wb')) -- open in "binary" mode
-  -- f:write(body)
-  -- f:close()
-  --
-  -- local file = io.open("sensordata_" .. year .. "_" .. month .. "_" .. day .. "_" .. hour .. ".csv", "rb")
-  -- local content = file:read("*a")
-  -- print(content)
+  local body, code = http.request("https://russellthackston.me/etl/sensordata_" .. year .. "_" .. month .. "_" .. day .. "_" .. hour .. ".csv")
+  if not body then
+    error(code)
+  end
+  local f = assert(io.open("sensordata_" .. year .. "_" .. month .. "_" .. day .. "_" .. hour .. ".csv", 'wb')) -- open in "binary" mode
+  f:write(body)
+  f:close()
+
+  local file = io.open("sensordata_" .. year .. "_" .. month .. "_" .. day .. "_" .. hour .. ".csv", "rb")
+  local content = file:read("*a")
+  print(content)
 
 
   return "Welcome to apps new " .. require("lapis.version")
 end)
 
-function download(url, file)
-  local content = http.get(url).readAll()
-  if not content then
-    error("Could not connect to website")
-  end
-  f = fs.open(file, "w")
-  f.write(content)
-  print(content);
-  f.close()
-end
 
 return app
