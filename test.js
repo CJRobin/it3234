cd11dfa9adfb4c93f832df0cb011ee56186c2bef
Corey Robinson <cojaro94@gmail.com>
2018-10-28 14:25:08 -0400

    f

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 var download = function(url, dest, cb) {
   var file = fs.createWriteStream(dest);
-  var request = http.get(url, function(response) {
+  var request = https.get(url, function(response) {
     response.pipe(file);
     file.on('finish', function() {
       file.close(cb);  // close() is async, call cb after close completes.
